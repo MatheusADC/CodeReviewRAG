a4ab112149f15cc9e0968feb14c055ff440c0946
Matheus Amaral da Costa <161625293+MatheusADC@users.noreply.github.com>
2025-04-21 10:59:08 -0300

    Add: Separação do documento em Chunks

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from langchain.chains import create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
 import os
 from git import Repo
-
+https://github.com/MatheusADC/CodeReviewRAG/blob/main/main.py
 repo_path = "./test_repo"
 
 repo = Repo.clone_from("https://github.com/langchain-ai/langchain", to_path=repo_path)
@@ -24,3 +24,12 @@ loader = GenericLoader.from_filesystem(
 
 documents = loader.load()
 len(documents)
+
+python_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=2000,
+    chunk_overlap=200,
+    language=Language.PYTHON
+)
+
+texts = python_splitter.split_documents(documents)
+len(texts)
